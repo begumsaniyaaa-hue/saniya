6849cf0da63c93ce60437159f90b29b5fb1aa551
Saniya Begum <begumsaniyaaa@gmail.com>
2026-09-22 16:25:07 +0530

    new file 2

---
+++ day.js
@@ -0,0 +1,31 @@
+function getDayName(day){
+    switch(day){
+        case 1:
+            return "Monday";
+            break;
+        case 2:
+            return "Tuesday";
+            break;
+        case 3:
+            return "Wednesday";
+            break;
+        case 4:
+            return "Thursday";
+             break;
+        case 5:
+            return "Friday";   
+            break;
+        case 6:
+            return "Saturday";
+            break;
+        case 7:
+            return "Sunday";
+            break;
+        default:
+            return "Invalid Number";
+        }
+
+}
+console.log(getDayName(1));
+console.log(getDayName(7));
+console.log(getDayName(8));
