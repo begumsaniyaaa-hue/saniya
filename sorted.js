638bc82a3a691263401223f32c43e34669247387
Saniya Begum <begumsaniyaaa@gmail.com>
2026-09-22 15:39:43 +0530

    new file js

---
+++ sorted.js
@@ -0,0 +1,18 @@
+function isSorted(arr){
+    let n=arr.length;
+    for(let i=0;i<n-1;i++){
+        
+        return false;
+    
+    
+}
+return false;
+}
+
+let arr1=[1,2,3,4,5]
+
+let arr2=[1,2,3,5,4]
+
+console.log(isSorted(arr1)); 
+
+console.log(isSorted(arr2));
---
+++ studentInfo.js
@@ -0,0 +1,17 @@
+let studentName = "Saniya";
+var age = 18;
+let course = "Bca";
+let collegeName = "SITM";
+let marks = 56;
+const birthYear = 2007;
+
+age = 19;
+marks = 66;
+
+
+console.log("Student Name",studentName);
+console.log("Age",age);
+console.log("Course",course);
+console.log("College Name",collegeName);
+console.log("Marks",marks);
+console.log("Birth Year",birthYear);
