9fa381c9e346facce336c3a8ea24b27ea7cace56
Saniya Begum <begumsaniyaaa@gmail.com>
2026-09-23 15:06:23 +0530

    target

---
+++ targer.js
@@ -0,0 +1,14 @@
+function findNumber(numbers,target){
+    let result="not found";
+    for (let i=0; i<numbers.length; i++){
+        if(numbers[i]<0){
+            continue;
+        }
+        if (numbers[i]===target){
+            result="found";
+            break;
+        }
+    }
+    return result;
+}
+console.log(findNumber([-5,10,-2,7,20],7))
